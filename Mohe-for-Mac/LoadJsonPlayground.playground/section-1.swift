a5ebc4b80e1d4f1b6e9ca1a81851d42d7954c3a1
Secbone <secbone@gmail.com>
2014-11-02 17:43:40 +0800

    read file path

--- Mohe-for-Mac/LoadJsonPlayground.playground/section-1.swift
+++ Mohe-for-Mac/LoadJsonPlayground.playground/section-1.swift
@@ -2,16 +2,28 @@
 
 import UIKit
 
-let urlString: String = "http://wowbox.duowan.com/wowplugin/AddonsUpdater.json"
+let urlString = "http://wowbox.duowan.com/wowplugin/AddonsUpdater.json"
 var url: NSURL = NSURL(string: urlString)!
 
 var data = NSData(contentsOfURL: url)
 
 var json: AnyObject? = NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.AllowFragments, error: nil)
 
-var pluginListAddr: String = json?.objectForKey("pluglistjsonaddr") as String
+var pluginListAddr: String = json?.objectForKey("plugjsonaddr") as String
 
 var pluginListData = NSData(contentsOfURL: NSURL(string: pluginListAddr)!)
+
+var crc32: NSDictionary = NSJSONSerialization.JSONObjectWithData(pluginListData!, options: NSJSONReadingOptions.AllowFragments, error: nil) as NSDictionary
+
+var list: NSArray = crc32["Crc32"] as NSArray
+
+for item in list{
+    var fileslist: NSArray = item["files"] as NSArray
+    for file in fileslist{
+        println(file["file"])
+    }
+}
+
 
 // TODO
 // convert Chinese from ASC to UTF-8
--- Mohe-for-Mac/ViewController.swift
+++ Mohe-for-Mac/ViewController.swift
@@ -11,8 +11,27 @@ import Cocoa
 class ViewController: NSViewController {
     
     @IBAction func getJson(sender: AnyObject){
-        let urlString: String = "http://wowbox.duowan.com/wowplugin/AddonsUpdater.json"
-        var url = NSURL(string: urlString)
+        let urlString = "http://wowbox.duowan.com/wowplugin/AddonsUpdater.json"
+        var url: NSURL = NSURL(string: urlString)!
+        
+        var data = NSData(contentsOfURL: url)
+        
+        var json: AnyObject? = NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.AllowFragments, error: nil)
+        
+        var pluginListAddr: String = json?.objectForKey("plugjsonaddr") as String
+        
+        var pluginListData = NSData(contentsOfURL: NSURL(string: pluginListAddr)!)
+        
+        var crc32: NSDictionary = NSJSONSerialization.JSONObjectWithData(pluginListData!, options: NSJSONReadingOptions.AllowFragments, error: nil) as NSDictionary
+        
+        var list: NSArray = crc32["Crc32"] as NSArray
+        
+        for item in list{
+            var fileslist: NSArray = item["files"] as NSArray
+            for file in fileslist{
+                println(file["file"])
+            }
+        }
     }
     
     override func viewDidLoad() {
