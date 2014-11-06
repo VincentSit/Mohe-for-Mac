d8d121bf7c370a1e3c815bcd84fad0af49413a99
Secbone <secbone@gmail.com>
2014-11-06 22:24:27 +0800

    fix download

--- Mohe-for-Mac/LoadJsonPlayground.playground/section-1.swift
+++ Mohe-for-Mac/LoadJsonPlayground.playground/section-1.swift
@@ -9,6 +9,8 @@ var data = NSData(contentsOfURL: url)
 
 var json: AnyObject? = NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.AllowFragments, error: nil)
 
+let pluginDownloadAddr: String = json?.objectForKey("plughttpaddr") as String
+
 var pluginListAddr: String = json?.objectForKey("plugjsonaddr") as String
 
 var pluginListData = NSData(contentsOfURL: NSURL(string: pluginListAddr)!)
@@ -17,14 +19,25 @@ var crc32: NSDictionary = NSJSONSerialization.JSONObjectWithData(pluginListData!
 
 var list: NSArray = crc32["Crc32"] as NSArray
 
-for item in list{
+/*for item in list{
     var folder: NSString = item["folder"] as NSString
     var fileslist: NSArray = item["files"] as NSArray
     for file in fileslist{
         var path: NSString = file["file"] as NSString
-        println("\(folder)/\(path)")
+        println("\(pluginDownloadAddr)\(folder)/\(path)")
     }
-}
+}*/
+
+var xml = "http://wowbox.duowan.com/wowplugin/AddOns/Accountant_Classic/Accountant.xml.7z"
+var xmlurl = NSURL(string: xml)
+var xmldata: NSData = NSData(contentsOfURL: xmlurl!)!
+
+var writePath = @"/Users/secbone/xml.7z"
+var bool = xmldata.writeToFile("/Users/secbone/xml.7z", atomically: true)
+
+
+
+
 
 
 // TODO
--- Mohe-for-Mac/ViewController.swift
+++ Mohe-for-Mac/ViewController.swift
@@ -10,6 +10,11 @@ import Cocoa
 
 class ViewController: NSViewController {
     
+    // test wow plugin path
+    let wowPath = "/User/secbone/Code/plugin/"
+    
+    var pluginDownloadAddr: String = ""
+    
     @IBAction func getJson(sender: AnyObject){
         let urlString = "http://wowbox.duowan.com/wowplugin/AddonsUpdater.json"
         var url: NSURL = NSURL(string: urlString)!
@@ -17,6 +22,8 @@ class ViewController: NSViewController {
         var data = NSData(contentsOfURL: url)
         
         var json: AnyObject? = NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.AllowFragments, error: nil)
+        
+        pluginDownloadAddr = json?.objectForKey("plughttpaddr") as String
         
         var pluginListAddr: String = json?.objectForKey("plugjsonaddr") as String
         
@@ -31,9 +38,20 @@ class ViewController: NSViewController {
             var fileslist: NSArray = item["files"] as NSArray
             for file in fileslist{
                 var path: NSString = file["file"] as NSString
-                println("\(folder)\\\(path)")
+                //println("\(pluginDownloadAddr)\(folder)/\(path)")
+                var filePath = "\(folder)\\\(path)"
+                println("\(filePath)")
+                write7zfile(filePath)
             }
         }
+        
+        var xml = "http://wowbox.duowan.com/wowplugin/AddOns/Accountant_Classic/Accountant.xml.7z"
+        var xmlurl = NSURL(string: xml)
+        var xmldata: NSData = NSData(contentsOfURL: xmlurl!)!
+        
+        //var writePath = @"/Users/secbone/xml.7z"
+        var bool = xmldata.writeToFile("/Users/secbone/xml.7z", atomically: true)
+        
     }
     
     override func viewDidLoad() {
@@ -50,6 +68,24 @@ class ViewController: NSViewController {
     
     func press(){
         println("press")
+    }
+    
+    func write7zfile(path: String) -> Bool? {
+        var downloadPath: String = "\(pluginDownloadAddr)\(path).7z"
+        downloadPath = replaceChar(downloadPath, searchString: "\\", replaceString: "/")
+        var pluginURL: NSURL = NSURL(string: downloadPath)!
+        var pluginData: NSData = NSData(contentsOfURL: pluginURL)!
+        var bool = pluginData.writeToFile("\(wowPath)\(path)", atomically: true)
+        return bool
+    }
+    
+    func replaceChar(string: NSString, searchString: String, replaceString: String) -> String {
+        var substr: NSRange = string.rangeOfString(searchString)
+        while(substr.location != NSNotFound){
+            string.replaceCharactersInRange(substr, withString:replaceString);
+            substr = string.rangeOfString(searchString)
+        }
+        return string
     }
 
 }
