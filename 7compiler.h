c33282e78904c09c343f8605c2e172dbbb9f4473
Secbone <secbone@gmail.com>
2014-11-08 21:18:50 +0800

    alpha 1.0

---
+++ 7compiler.h
@@ -0,0 +1,12 @@
+//
+//  7compiler.h
+//  Mohe-for-Mac
+//
+//  Created by Secbone on 11/8/14.
+//  Copyright (c) 2014 Secbone. All rights reserved.
+//
+
+
+
+#import "LZMAExtractor.h"
+
--- Mohe-for-Mac/ViewController.swift
+++ Mohe-for-Mac/ViewController.swift
@@ -11,9 +11,33 @@ import Cocoa
 class ViewController: NSViewController {
     
     // test wow plugin path
-    let wowPath = "/Users/secbone/Code/plugin/"
+    var wowPath = "/Applications/World of Warcraft/Interface/AddOns/"
+    var AddOnsPath = "Interface/AddOns"
+    let tempPath = NSTemporaryDirectory()
     
     var pluginDownloadAddr: String = ""
+    
+
+    
+    @IBOutlet var updatingLabel: NSTextField!
+    @IBOutlet var wowPathTextField: NSTextField!
+    @IBAction func chooseDirectory(sender: AnyObject) {
+        var panel: NSOpenPanel = NSOpenPanel()
+        panel.prompt = "Choose"
+        panel.showsResizeIndicator = true
+        panel.canChooseFiles = false
+        panel.canChooseDirectories = true
+        //panel.allowsMultipleSelection = false
+        panel.beginWithCompletionHandler({(result) -> Void in
+            if result == NSOKButton {
+                var selection: NSURL = panel.URLs[0] as NSURL
+                var path = selection.path!
+                self.wowPathTextField.stringValue = path
+                self.wowPath = path.stringByAppendingPathComponent(self.AddOnsPath)
+            }
+        })
+        
+    }
     
     @IBAction func getJson(sender: AnyObject){
         let urlString = "http://wowbox.duowan.com/wowplugin/AddonsUpdater.json"
@@ -39,18 +63,31 @@ class ViewController: NSViewController {
             for file in fileslist{
                 var path: NSString = file["file"] as NSString
                 //println("\(pluginDownloadAddr)\(folder)/\(path)")
-                var filePath = "\(folder)\\\(path)"
+                var filePath = folder.stringByAppendingPathComponent(path)
                 //println("\(filePath)")
+                    self.updatingLabel.stringValue = "Updating \(path)"
                 write7zfile(filePath)
             }
         }
         
-        var xml = "http://wowbox.duowan.com/wowplugin/AddOns/Accountant_Classic/Accountant.xml.7z"
+        self.updatingLabel.stringValue = "Update Done! Enjoy It!"
+        
+        
+        // Test download 7z file
+        /*var xml = "http://wowbox.duowan.com/wowplugin/AddOns/Accountant_Classic/Accountant.xml.7z"
         var xmlurl = NSURL(string: xml)
         var xmldata: NSData = NSData(contentsOfURL: xmlurl!)!
         
         //var writePath = @"/Users/secbone/xml.7z"
-        var bool = xmldata.writeToFile("/Users/secbone/xml.7z", atomically: true)
+        var bool = xmldata.writeToFile("/Users/secbone/xml.7z", atomically: true)*/
+        
+        //Test decompile 7z file
+        
+        //var srcPath = "/Users/secbone/xml.7z" as NSString
+        //var toPath = "/Users/secbone/Public" as NSString
+        //var array = decompile7z(srcPath, toPath: toPath)
+        //println(array)
+        
         
     }
     
@@ -66,46 +103,74 @@ class ViewController: NSViewController {
         }
     }
     
-    func press(){
-        println("press")
-    }
     
-    func write7zfile(path: String) -> Bool? {
+    func write7zfile(path: NSString) -> Bool? {
+        
+        var folderPath = replaceChar(path.mutableCopy() as NSMutableString, searchString: "\\", replaceString: "/")
         
         //get download Path
-        let downloadString = "\(pluginDownloadAddr)\(path).7z" as NSString
-        var downloadPath: NSMutableString = downloadString.mutableCopy() as NSMutableString
+        let downloadString = "\(pluginDownloadAddr.stringByAppendingPathComponent(folderPath)).7z" as NSString
+        //var downloadPath: NSMutableString = downloadString.mutableCopy() as NSMutableString
         
         //replace \ to /
-        downloadPath = replaceChar(downloadPath, searchString: "\\", replaceString: "/")
+        //downloadPath = replaceChar(downloadPath, searchString: "\\", replaceString: "/")
         
         //get file data
-        var pluginURL: NSURL = NSURL(string: downloadPath)!
+        var pluginURL: NSURL = NSURL(string: downloadString.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!)!
         var pluginData: NSData = NSData(contentsOfURL: pluginURL)!
         
-        //get write path
-        var pluginWritePathNS = "\(wowPath)\(path).7z" as NSString
-        var pluginWritePath = pluginWritePathNS.mutableCopy() as NSMutableString
+        //get write file
+        var pluginWritePathNS = "\(tempPath.stringByAppendingPathComponent(folderPath)).7z" as NSString
+        var bool = writeFileWithPath(pluginWritePathNS, fileData: pluginData)
+        bool = decompile7z(pluginWritePathNS, toPath: "\(wowPath.stringByAppendingPathComponent(getFilePath(folderPath)))")
+        return bool
+    }
+    
+    
+    /*************************
+    ** Write File With Path **
+    **************************/
+    func writeFileWithPath(path: NSString, fileData: NSData) -> Bool {
+        
+        var writePath = path.mutableCopy() as NSMutableString
         
         //replace \ to /
-        pluginWritePath = replaceChar(pluginWritePath, searchString: "\\", replaceString: "/")
-        //println(pluginWritePath)
+        writePath = replaceChar(writePath, searchString: "\\", replaceString: "/")
         
         //get folder path
-        var pathArray: NSArray = pluginWritePath.pathComponents
-        //println(pathArray)
-        var folderPath = NSString.pathWithComponents(pathArray.subarrayWithRange(NSMakeRange(0, pathArray.count-1)))
-        println(folderPath)
+        var folderPath = getFilePath(writePath)
         
         //create folder and write file
         var fileManager = NSFileManager()
         var bool = fileManager.createDirectoryAtPath(folderPath, withIntermediateDirectories: true, attributes: nil, error: nil)
-        bool = fileManager.createFileAtPath(pluginWritePath, contents: pluginData, attributes: nil)
-        //var bool = pluginData.writeToFile(pluginWritePath, atomically: true)
-        println(bool)
+        bool = fileManager.createFileAtPath(writePath, contents: fileData, attributes: nil)
+        //println(bool)
         
         
         return bool
+    }
+    
+    func decompile7z(filePath: NSString, toPath: NSString) -> Bool {
+        var pathArray: NSArray = LZMAExtractor.extract7zArchive(filePath, tmpDirName: toPath)
+        var bool = false
+        for tmpPath in pathArray {
+            var filedata: NSData = NSData(contentsOfFile: tmpPath as NSString)!
+            var fileName = getFileName(tmpPath as NSString)
+            var writePath = toPath.stringByAppendingPathComponent(fileName)
+            bool = writeFileWithPath(writePath, fileData: filedata)
+        }
+        return bool
+    }
+    
+    func getFileName(filePath: NSString) -> NSString {
+        var fileName = filePath.lastPathComponent
+        return fileName
+    }
+    
+    func getFilePath(filePath: NSString) -> NSString {
+        var pathArray: NSArray = filePath.pathComponents
+        var folderPath = NSString.pathWithComponents(pathArray.subarrayWithRange(NSMakeRange(0, pathArray.count-1)))
+        return folderPath
     }
     
     func replaceChar(string: NSMutableString, searchString: String, replaceString: String) -> NSMutableString {
